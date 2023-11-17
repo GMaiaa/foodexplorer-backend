3875cd2f035597b21645a977ef209dabd9af80d0
GMaiaa <gabrielmaiaa22@gmail.com>
2023-11-16 22:50:31 -0300

    Food Explorer Backend Done

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+// Importando da biblioteca
+const { Router } = require('express');
+
+const usersRoutes = require('./users.routes');
+const dishesRoutes = require('./dishes.routes')
+const dishesAdminRoutes = require('./dishesAdmin.routes')
+const sessionsRoutes = require('./sessions.routes')
+
+const routes = Router();
+
+routes.use('/users', usersRoutes);
+routes.use('/dishes', dishesRoutes);
+routes.use('/adminDishes', dishesAdminRoutes);
+routes.use('/sessions', sessionsRoutes);
+
+module.exports = routes;
---
+++ src/routes/users.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const multer = require('multer');
+
+const UsersController = require('../controllers/UsersController');
+
+const usersController = new UsersController();
+
+const ensureAuthenticated = require("../middleware/ensureAuthenticated")
+
+const usersRoutes = Router();
+
+
+usersRoutes.post('/', usersController.create)
+usersRoutes.put('/', ensureAuthenticated, usersController.update)
+
+
+module.exports = usersRoutes;
